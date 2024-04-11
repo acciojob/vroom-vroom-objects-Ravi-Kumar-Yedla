493068eb24a1942de97ba6f292415980f02789ab
Ravi-Kumar-Yedla <136640114+Ravi-Kumar-Yedla@users.noreply.github.com>
2024-04-11 09:50:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,47 @@
-// Complete the js code
+// // Complete the js code
 function Car(make, model) {
-         this.make=make;
-	     this.model=model;
-}
-	Car.prototype.getMakeModel = function() {	
-		return  this.make + " " + this.model;
+  this.make = make;
+  this.model = model;
 }
 
+// Add method to Car prototype
+Car.prototype.getMakeModel = function() {
+  return `${this.make} ${this.model}`;
+};
+
+// SportsCar constructor function
 function SportsCar(make, model, topSpeed) {
-	Car.call(this, make, model);
-	this.topSpeed = topSpeed;
+  Car.call(this, make, model); // Call Car constructor to set make and model
+  this.topSpeed = topSpeed;
 }
-SportsCar.prototype = object.create(Car.prototype);
+
+// Inherit Car prototype
+SportsCar.prototype = Object.create(Car.prototype);
 SportsCar.prototype.constructor = SportsCar;
 
-   SportsCar.prototype.getTopSpeed = function(){
-	   return this.topSpeed;
-   }
+// Add method to SportsCar prototype
+SportsCar.prototype.getTopSpeed = function() {
+  return this.topSpeed;
+};
+
+// function Car(make, model) {
+//          this.make=make;
+// 	     this.model=model;
+// }
+// 	Car.prototype.getMakeModel = function() {	
+// 		return  this.make + " " + this.model;
+// }
+
+// function SportsCar(make, model, topSpeed) {
+// 	Car.call(this, make, model);
+// 	this.topSpeed = topSpeed;
+// }
+// SportsCar.prototype = object.create(Car.prototype);
+// SportsCar.prototype.constructor = SportsCar;
+
+//    SportsCar.prototype.getTopSpeed = function(){
+// 	   return this.topSpeed;
+//    }
 // Do not change the code below
 window.Car = Car;
 window.SportsCar = SportsCar;
